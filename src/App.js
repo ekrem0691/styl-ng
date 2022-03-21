867bfe67e71d672f8d001ee08bd31e066a32d9b2
ekrem0691 <06hilalekrem06@gmail.com>
2022-03-22 02:30:29 +0300

    2. session yapıldı

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Card from "./components/card/Card";
+import Inline from "./components/inline-style/Inline";
+import ModuleBtn from "./components/moduleCss/ModuleBtn";
+import StyleSheet from "./components/regular-style/StyleSheet";
+import data from "./data";
+const App = () => {
+  return (
+    <>
+ <Inline/>
+    <StyleSheet secenek={false}/>
+  <Card veri={data}/>
+  <h1>ekooooo</h1>
+<ModuleBtn btnStyle="birinci" btnName="Kaydet"/>
+<ModuleBtn btnStyle="ikinci" btnName="Sil"/>  
+
+    </>
+  );
+};
+
+export default App;
